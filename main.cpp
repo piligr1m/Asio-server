9117d5d92b04f19ce1947ff107ff0732c5cc375f
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2019-12-15 17:51:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <boost/asio.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
+#include <iostream>
 using namespace boost::asio;
 using namespace boost::posix_time;
 io_service service;
